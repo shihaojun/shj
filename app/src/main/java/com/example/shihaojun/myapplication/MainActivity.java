7600fb8a1b8d625b579464b0808062947ca7f75c
shihaojun <shi_haojun@qq.com>
2017-06-07 15:38:32 +0800

    second commit change

--- app/src/main/java/com/example/shihaojun/myapplication/MainActivity.java
+++ app/src/main/java/com/example/shihaojun/myapplication/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+//first change test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
